a88db73f7b40fe0943d3ee062ca710eb14595784
PierAlbertoPierini <pieralbertopierini@gmail.com>
2018-03-13 23:27:36 -0700

    Starting point for usage.rb

---
+++ src/usage.rb
@@ -0,0 +1,24 @@
+text = WordsOnlySanitizer.to_words(File.read('words.txt'))
+filter = WordsOnlySanitizer.to_words(File.read('filter_words.txt'))
+analyser = Analyser.new(text, filter)
+
+puts "Word count after filtering is: #{analyser.word_count}"
+puts "\n"
+
+puts "The most frequent words are:"
+analyser.highest_occurring_words.each do |key, value|
+  puts "  - #{key}: #{value} occurences"
+end
+puts "\n"
+
+puts "The longest words are:"
+analyser.longest_words.each do |word|
+  puts "  - #{word.first}: #{word.last} characters"
+end
+puts "\n"
+
+puts "Word list:"
+puts analyser.html_list
+
+puts "JSON object:"
+puts analyser.json_list

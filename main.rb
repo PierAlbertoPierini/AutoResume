653d9ef534084d3a829194eaecc497ebcdcd77a0
PierAlbertoPierini <pieralbertopierini@gmail.com>
2018-03-15 00:08:00 -0700

    Started top play with word_counter gem

--- main.rb
+++ main.rb
@@ -24,7 +24,7 @@
 
 require 'tk'
 require 'tkextlib/tile'
-require_relative 'src/word_counter.rb'
+require 'words_counted'
 
 # Start Root window mainloop
 $root = TkRoot.new( :title => "AutoResume", :width => 640, :height => 480)
@@ -169,7 +169,7 @@ Tk::Tile::Button.new(f1) do
 
 Tk::Tile::Button.new(f1) do
 	text 'Count Words'
-	command proc{puts count_words(my_resume.get("1.0", 'end'))}
+	command proc{counter = WordsCounted::Tokeniser(my_resume.get("1.0", 'end')).tokenise(exclude: "text"); puts counter.token_frequency}
   grid( :column => 0, :row => 1, :sticky => 'e')
 end
 
@@ -197,7 +197,7 @@ label_2.grid :column => 6, :row => 0
 
 Tk::Tile::Button.new(f1) do
 	text 'Count Words'
-	command proc{puts "ciao"; puts count_words(job_application.get("1.0", 'end'))}
+	command proc{puts WordsCounted.count(job_application.get("1.0", 'end'))}
   grid( :column => 2, :row => 1, :sticky => 'e')
 end
 
